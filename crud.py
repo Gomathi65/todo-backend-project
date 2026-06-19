d6241d0f9534285dd4c830da132931750cb362c1
Gomathi65 <shiva20gm@gmail.com>
2026-06-19 20:35:07 +0530

    Update crud logic

--- crud.py
+++ crud.py
@@ -1,10 +1,4 @@
 from sqlalchemy.orm import Session
-import models
-import schemas
-
-
-def create_todo(db: Session, todo: schemas.TodoCreate):
-    db_todo = models.Todo(
 from models import Todo
 from schemas import TodoCreate, TodoUpdate
 
@@ -20,22 +14,6 @@ def create_todo(db: Session, todo: TodoCreate):
     return db_todo
 
 
-def get_todos(db: Session):
-    return db.query(models.Todo).all()
-
-
-def get_todo_by_id(db: Session, todo_id: int):
-    return db.query(models.Todo).filter(
-        models.Todo.id == todo_id
-    ).first()
-
-
-def update_todo(
-    db: Session,
-    todo_id: int,
-    todo: schemas.TodoUpdate
-):
-    db_todo = get_todo_by_id(db, todo_id)
 def get_todos(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Todo).offset(skip).limit(limit).all()
 
@@ -54,11 +32,6 @@ def update_todo(db: Session, todo_id: int, todo_update: TodoUpdate):
     if not db_todo:
         return None
 
-    db_todo.completed = todo.completed
-
-    db.commit()
-    db.refresh(db_todo)
-
     update_data = todo_update.model_dump(exclude_unset=True)
 
     for key, value in update_data.items():
@@ -70,7 +43,6 @@ def update_todo(db: Session, todo_id: int, todo_update: TodoUpdate):
 
 
 def delete_todo(db: Session, todo_id: int):
-    db_todo = get_todo_by_id(db, todo_id)
     db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
 
     if not db_todo:
@@ -78,6 +50,4 @@ def delete_todo(db: Session, todo_id: int):
 
     db.delete(db_todo)
     db.commit()
-
-    return True
     return db_todo
